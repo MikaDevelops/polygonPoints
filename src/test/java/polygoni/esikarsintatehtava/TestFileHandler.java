92e51501bf82fb8fd9993b6c51c78b04bb7576ba
mikavirtala <mika.1.virtala@edu.karelia.fi>
2022-08-28 13:55:18 +0300

    created test class TestFileHandler

---
+++ src/test/java/polygoni/esikarsintatehtava/TestFileHandler.java
@@ -0,0 +1,14 @@
+package polygoni.esikarsintatehtava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFileHandler {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
